93e4be509a8317eb1c35a1d1fcb811db1452df64
Kuba <jakub.tarnacki1@gmail.com>
2025-10-14 13:20:44 +0200

    Initial commit: Setup project with RainbowKit and form UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// pages/_app.tsx
+import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiConfig } from 'wagmi';
+import { config } from '@/wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // <-- 1. Import
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+// 2. Stwórz instancję klienta POZA komponentem
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // 3. Owiń wszystko w QueryClientProvider
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig config={config}>
+        <RainbowKitProvider>
+          <Head>
+            <title>Generator Kontraktów NFT</title>
+          </Head>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </QueryClientProvider>
+  );
 }
---
+++ src/wagmi.ts
@@ -0,0 +1,33 @@
+// src/wagmi.ts
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { http, createConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  goerli,
+} from 'wagmi/chains';
+
+
+const chains = [mainnet, polygon, optimism, arbitrum, base, goerli] as const;
+
+
+const { connectors } = getDefaultWallets({
+  appName: 'NFT Contract Generator App',
+  projectId: 'a728b5819ee7d003535812f57f75bfb0',
+});
+
+export const config = createConfig({
+  chains: chains,
+  connectors,
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+    [base.id]: http(),
+    [goerli.id]: http(),
+  },
+});
